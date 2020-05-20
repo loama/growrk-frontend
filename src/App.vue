f5e947c4259aecaf9816e5a077307fc3a1e5f15d
Jaime López <loama18@gmail.com>
2020-05-20 14:09:01 -0500

    init vue project

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,24 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <Pricing />
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import Pricing from './components/Pricing.vue'
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+export default {
+  components: {
+    Pricing: Pricing
   }
 }
+</script>
+
+<style lang="sass">
+html, body
+  background: #F7F7FF
+  margin: 0
+  padding: 0
+
+#app
 </style>
---
+++ src/components/Pricing.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="pricing">
+    pricing
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
